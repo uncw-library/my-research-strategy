4f84714f30d705ea64c6853d6613992f8c2302c6
corbettg <corbettg@uncw.edu>
2018-07-09 09:16:11 -0400

    npm_and_video_updates

--- src/Componenets/Instructions.js
+++ src/Componenets/Instructions.js
@@ -4,7 +4,7 @@ import { Grid, Typography } from 'material-ui'
 export default ({ activeView, activeDireactions}) =>
 <Grid container style={{marginTop:30, marginBottom:30}}>
     <Grid item xl={5} lg={6} md={6} sm={12} xs={12}>
-       {activeView === 'search' ?
+       {(activeView === 'createQuestion' || activeView === 'search') ?
        <iframe className="tscplayer_inline embeddedObject" name="tsc_player" scrolling="no" frameBorder="0" type="text/html"
                style={{overflow:'hidden'}} src="https://www.screencast.com/users/Randall_Library/folders/Tammy uploads/media/70eca49f-40d1-4752-94f9-0d9165a4e93c/embed"
                height="300" width="95%" title="Research">
